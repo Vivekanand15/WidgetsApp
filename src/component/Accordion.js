9776fb51aad5e4e22aa875f183b1820ee911ed25
Vivekanand <vivekanand.v@surya-soft.com>
2022-02-24 05:16:34 +0530

    css is applied

--- src/component/Accordion.js
+++ src/component/Accordion.js
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Accordion=({items})=>{
-    return <h1>{items.length}</h1>
+    const renderedItems=items.map(item=>{
+        return <div key={item.title}>
+            <div className="title active">
+            <i className="dropdown icon"></i>
+            {item.title}
+        </div>
+        <div className="content active">
+            <p>{item.content}</p>
+        </div>
+        </div>
+    });
+    return <div className="ui styled accordion">
+        {renderedItems}
+    </div>
 }
 export default Accordion;
