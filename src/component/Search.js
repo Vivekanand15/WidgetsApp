3a0ddbf2bb92f5389da76d241722bbf725965091
Vivekanand <vivekanand.v@surya-soft.com>
2022-02-25 19:21:16 +0530

    Search component is Ready

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,8 +1,48 @@
-import React,{ useState } from 'react';
+import React,{ useState , useEffect} from 'react';
+import axios from 'axios';
 
 const Search =()=>{
     const [term,setTerm]=useState("");
-    return (
+    const [results,setResults]=useState([]);
+    const resultRender=results.map((result)=>{
+        return (
+            <div key={result.pageid} className="item">
+                 <div className="right floated content">
+                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+                </div>
+                <div className="content">
+                    <div className="headers">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{__html:result.snippet}} ></span>
+                </div>
+            </div>
+        )
+    });
+    useEffect( ()=>{
+        const search=async ()=>{
+            const {data}=await axios.get('https://en.wikipedia.org/w/api.php',{
+                params:{
+                    action:'query',
+                    list:'search',
+                    origin:'*',
+                    format:'json',
+                    srsearch:term,
+                }
+            });
+            setResults(data.query.search);
+        };
+        const timeoutid=setTimeout(() => {
+            if(term){
+                search();
+            }
+        }, 1000);
+        return () =>{
+            clearTimeout(timeoutid);
+        };
+    },[term]);
+    
+    return(
         <div>
             <div className="ui form ">
                 <div className='field'>
@@ -14,8 +54,11 @@ const Search =()=>{
                      />
                 </div>
             </div>
+            <div className='ui celled list'>
+                {resultRender}
+            </div>
         </div>
-    )
+    );
 }
 
 export default Search;
